edeaa7eac2845300cd8bb98b29f157db9a6c759f
Keely Bailey <keelybailey3@gmail.com>
2021-06-08 22:34:20 -0500

    Start updating HTMl & create javascript file

---
+++ script.js
@@ -0,0 +1,17 @@
+//objects
+
+//what functions do I ~want~ to ~create~? 
+
+// Use event bubbling instead of doing individual lines for each button
+Object.addEventListener('click', event => {
+    console.log(event.target.innerText)
+})
+
+
+
+
+
+//when to reset to default
+
+
+

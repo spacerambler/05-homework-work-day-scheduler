fc0352a65e3ad6afd1e226f801d50ec564fd70c4
Keely Bailey <keelybailey3@gmail.com>
2021-07-28 20:21:31 -0500

    Add notes on what I want to do next

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 //objects
 var dateEl = $('#currentDay')
+var timeBlockEl = $('#7')
 
 //Inputting text for the current day in heading
 var now = moment().format("dddd, MMMM Do YYYY")
 
 dateEl.text(`${now}`)
+
+//highlight current time
+// timeBlockEl.attr('class', 'present')
+
+function highlightTime(){
+    //if current time === id # then add class of present
+    //if current time < id then add class of past
+    //else class of present
+
+}
+
+//local storage, save inputs
